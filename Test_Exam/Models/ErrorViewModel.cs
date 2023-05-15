33565f47fa9b33e06d5c47f18c6616c2893de249
wehba-dot <cph-wk7@cphbusiness.dk>
2023-05-15 11:11:40 +0200

    first commit

---
+++ Test_Exam/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_Exam.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
